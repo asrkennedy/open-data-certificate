e68b9cc499650265fcc3d261049e588db93c1f10
Andrea Kennedy <asrkennedy@gmail.com>
2013-08-16 16:22:34 +0100

    replaces accidentally overwritten test for 4 complete answer model tests

--- test/unit/answer_test.rb
+++ test/unit/answer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
 
-  test "requirement should equal its question's requirement if it doesn't have its own" do
+    test "requirement should equal its question's requirement if it doesn't have its own" do
     question = FactoryGirl.create :required_question
     answer = FactoryGirl.create :answer, question: question
 
@@ -16,18 +16,15 @@ class AnswerTest < ActiveSupport::TestCase
   end
 
   test "requirement_level should equal assigned value" do
+    answer = FactoryGirl.create :answer_with_requirement
+
+    assert_equal answer.requirement_level, "pilot"
+    end
+
+  test "requirement_level should equal assigned value" do
     answer = FactoryGirl.create :answer_with_requirement_as_array
     # what should the array return as?
     assert_equal answer.requirement_level, "pilot, basic"
   end
 
-
-
 end
-
-
-
-
-  # test 'statement_text uses existing to_formatted_s' do
-  #   assert_equal 'yes, I do', @response1.statement_text
-  # end
